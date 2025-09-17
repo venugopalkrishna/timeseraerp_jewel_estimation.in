bf8ae519c120d9d016137096eccd730df0fe0c4d
venugopalkrishna <tumativenugopalakrishna@gmail.com>
2025-09-17 18:41:11 +0530

    vn/updated printer 1

--- src/Components/BarCodeCheck/eposPrint.jsx
+++ src/Components/BarCodeCheck/eposPrint.jsx
@@ -16,19 +16,8 @@ export const printReceipt = (
   var printer = null;
   var ePosDev = new window.epson.ePOSDevice();
 
-  let url;
-  try {
-    url = new URL(
-      printerIP.includes("://") ? printerIP : `http://${printerIP}`
-    );
-  } catch (err) {
-    console.error("Invalid printerIP:", printerIP);
-    return;
-  }
-
-  const secure = url.protocol === "https:";
-
-  ePosDev.connect(url.hostname, url.port || 8008, cbConnect, { secure });
+  // ePosDev.connect(printerIP, 8008, cbConnect);
+  ePosDev.connect(printerIP, 8043, cbConnect, { secure: true });
 
   function cbConnect(data) {
     if (data === "OK") {
--- src/Components/BarCodeCheck/eposPrintModel2.jsx
+++ src/Components/BarCodeCheck/eposPrintModel2.jsx
@@ -16,19 +16,7 @@ export const printReceiptModule2 = (
   var printer = null;
   var ePosDev = new window.epson.ePOSDevice();
 
-  let url;
-  try {
-    url = new URL(
-      printerIP.includes("://") ? printerIP : `http://${printerIP}`
-    );
-  } catch (err) {
-    console.error("Invalid printerIP:", printerIP);
-    return;
-  }
-
-  const secure = url.protocol === "https:";
-
-  ePosDev.connect(url.hostname, url.port || 8008, cbConnect, { secure });
+  ePosDev.connect(printerIP, 8008, cbConnect);
 
   function cbConnect(data) {
     if (data === "OK") {
